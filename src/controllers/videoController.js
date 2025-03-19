ac2879e641cd38547586cf1dc48a7d7566de2dbd
Duzzyn <eduardonense05@gmail.com>
2025-03-19 19:15:46 -0300

    :sparkles: Feat: Post Default value

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize')
 const video = require('../models');
 
 class VideoController {
@@ -21,8 +22,14 @@ class VideoController {
     }
 
     async criaVideo(req, res) {
-        const dados = req.body;
+        const { titulo, descricao, url, categoriaId } = req.body;
         try {
+            const dados = {
+                titulo,
+                descricao,
+                url,
+                categoriaId: categoriaId || 16,
+            }
             const criaVideoRegistro = await video.Video.create(dados)
             return res.status(201).json(criaVideoRegistro)
         } catch (erro) {
@@ -46,6 +53,31 @@ class VideoController {
         try {
             await video.Video.destroy({ where: { id } })
             return res.status(200).json({ mensagem: `Id ${id} Deletado com Sucesso` })
+        } catch (erro) {
+            return res.status(500).json({ erro: erro.message })
+        }
+    }
+
+    async pegaVideosPorCategoria(req, res) {
+        const { categoriaId } = req.params;
+        try {
+            const buscaVideoCategoria = await video.Video.findAll({ where: { categoriaId } })
+            return res.status(200).json(buscaVideoCategoria)
+        } catch (erro) {
+            return res.status(500).json({ erro: erro.message })
+        }
+    }
+
+    async pegaVideosPorTitulo(req, res) {
+        const { titulo } = req.query;
+        try {
+            const buscaVideoTitulo = await video.Video.findAll({
+                where:
+                {
+                    titulo: { [Op.like]: `%${titulo}%` }
+                }
+            })
+            return res.status(200).json(buscaVideoTitulo)
         } catch (erro) {
             return res.status(500).json({ erro: erro.message })
         }
